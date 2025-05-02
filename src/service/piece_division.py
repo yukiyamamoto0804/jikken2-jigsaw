1da29c743feb212d3af32fee5d38cd4380128904
yamamotoyuki <103938277+yukiyamamoto0804@users.noreply.github.com>
2025-05-02 15:07:29 +0900

    piece_divisionの実行方法の変更

--- src/service/piece_division.py
+++ src/service/piece_division.py
@@ -9,18 +9,31 @@ from PIL import Image
 class PieceDivision:
     def __init__(
         self,
-        input_dir="data/puzzle_pieces",
+        multi_input_dir="data/puzzle_pieces",
+        single_input_dir="data/single_piece",
         output_dir="data/piece_transparent",
         debug=False,
     ):
-        self.input_dir = Path(input_dir)
+        self.multi_input_dir = Path(multi_input_dir)
+        self.single_input_dir = Path(single_input_dir)
         self.output_dir = Path(output_dir)
         self.debug = debug
-        self.input_dir.mkdir(parents=True, exist_ok=True)
+        self.multi_input_dir.mkdir(parents=True, exist_ok=True)
+        self.single_input_dir.mkdir(parents=True, exist_ok=True)
         self.output_dir.mkdir(parents=True, exist_ok=True)
+    
+    def process_init(self):
+        self.idx = 0
+    
+    def extract_multi_pieces(self, piece_id):
+        image_path = self.multi_input_dir / f"{piece_id}.png"
+        self.extract_pieces_masked(image_path)
+    
+    def extract_single_piece(self, piece_id, img_id):
+        image_path = self.single_input_dir / f"{piece_id}_{img_id}.png"
+        self.extract_pieces_masked(image_path)
 
-    def extract_pieces_masked(self, piece_id, work_short_edge=800):
-        image_path = self.input_dir / f"{piece_id}.png"
+    def extract_pieces_masked(self, image_path, work_short_edge=800):
         # 1. 画像読み込み & リサイズ（作業用画像）
         img_full = cv2.imread(image_path)
         h_full, w_full = img_full.shape[:2]
@@ -78,8 +91,9 @@ class PieceDivision:
             alpha = mask_cropped  # 255: ピース, 0: 透明
             rgba = np.dstack([roi_rgb, alpha])
 
+            self.idx += 1
             idx += 1
-            out_path = self.output_dir / f"piece_{idx:03d}.png"
+            out_path = self.output_dir / f"piece_{self.idx:03d}.png"
             Image.fromarray(rgba).save(out_path)
 
         print(f"{idx} 個の透明背景付きピースを切り出して保存しました。")
@@ -87,4 +101,6 @@ class PieceDivision:
 
 if __name__ == "__main__":
     piece_division = PieceDivision(debug=False)
-    piece_division.extract_pieces_masked("piece")
+    piece_division.process_init()
+    piece_division.extract_multi_pieces("piece")
+    piece_division.extract_single_piece("piece", "1")
